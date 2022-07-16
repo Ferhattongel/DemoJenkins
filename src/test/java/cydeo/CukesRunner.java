97469f9927fe89015ada2e8a0242027411a9d50f
ferhattongel <ferhattongel@gmail.com>
2022-07-16 10:44:27 -0400

    Triggered

--- src/test/java/cydeo/CukesRunner.java
+++ src/test/java/cydeo/CukesRunner.java
@@ -7,9 +7,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-//                "json:target/cucumber.json",
-//                "html:target/cucumber/report.html",
-//                "junit:target/junit/junit-report.xml",
+               "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+               "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",},
         features = "src/test/resources/features",
         publish = true
